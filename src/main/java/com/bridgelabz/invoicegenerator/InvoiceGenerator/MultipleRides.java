b2347f48d2e2b0687a5772972082ff8e211a6fe4
DuvvuruPavithra <duvvurupavithra28@gmail.com>
2022-02-15 01:06:22 +0530

    Added: The invoice Generator Takes Multiple rides and Calculatinh the Total Fare is added successfully

---
+++ src/main/java/com/bridgelabz/invoicegenerator/InvoiceGenerator/MultipleRides.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.invoicegenerator.InvoiceGenerator;
+
+public class MultipleRides {
+	
+	public double distance;
+	public int time;
+
+	public MultipleRides(double distance,int time)
+	{
+		this.distance = distance;
+		this.time = time; 
+	}
+}
